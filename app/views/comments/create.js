8f639b17abffe1c1f3e91cfef2664974276539e5
mahdi <mahdi.hassanzadeh66@gmail.com>
2012-10-05 02:08:55 -0500

    fix the reload

--- app/views/comments/create.js
+++ app/views/comments/create.js
@@ -1,3 +1,3 @@
-$('#list_line_even').html("<%= escape_javascript(render(:partial => @page.comments)) %>");
+$('#show_comment').html("<%= escape_javascript(render(:partial => @page.comments)) %>");
 $(":input:not(input[type=submit])").val(" ");
 
